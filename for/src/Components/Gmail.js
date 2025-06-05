fe6d52aa9c654398ee3930be33da74665668f52d
Aayush mishra <zccz2066@gmail.com>
2025-06-05 11:24:09 +0530

    Update Gmail.js

--- for/src/Components/Gmail.js
+++ for/src/Components/Gmail.js
@@ -18,7 +18,7 @@ const Gmail = () => {
         if(gmail)
         {
           try {
-      const res = await fetch("https://theme-backend-8l9z.onrender.com/api/auth/gmailOtp", { method: "POST",headers: { "Content-Type": "application/json" },body: JSON.stringify({gmail}), });
+      const res = await fetch("http://localhost:5000/api/auth/gmailOtp", { method: "POST",headers: { "Content-Type": "application/json" },body: JSON.stringify({gmail}), });
 
       const data = await res.json();
         console.log(data);
